4739f7ecc7b728397e6df76287616d3b9b70db3e
Lucas Suárez <lsuarezc.zentagroup@cmpc.cl>
2023-08-15 00:42:35 -0400

    fix code format

--- day_1.c
+++ day_1.c
@@ -12,8 +12,8 @@ int main()
     size_t len = 100;
     ssize_t read;
     int status = ON;
-	int max_sum = 0;
-	int partial_sum = 0;
+    int max_sum = 0;
+    int partial_sum = 0;
     int num;
 
     fp = fopen("input.txt", "r");
@@ -21,27 +21,26 @@ int main()
         exit(EXIT_FAILURE);
     }
     while ((read = getline(&line, &len, fp)) != -1) {
-		if (strcmp(line, "\n") == 0){
+        if (strcmp(line, "\n") == 0){
             if (status == ON){
-		        status = OFF;
-				if (partial_sum > max_sum){
-				    max_sum = partial_sum;
-				}
-			}
-		} else {
-		    if (status == OFF){
-		        status = ON;
-				partial_sum = 0;
-			}
-		}
-		
-		if (status == ON){
-		    num = atoi(line);
-			partial_sum += num;
-		}
+                status = OFF;
+                if (partial_sum > max_sum){
+                    max_sum = partial_sum;
+                }
+            }
+        } else {
+            if (status == OFF){
+                status = ON;
+                partial_sum = 0;
+            }
+        }
+        if (status == ON){
+            num = atoi(line);
+            partial_sum += num;
+        }
 
     }
-	printf("Solution: %d\n", max_sum);
+    printf("Solution: %d\n", max_sum);
     fclose(fp);
     if (line)
         free(line);
